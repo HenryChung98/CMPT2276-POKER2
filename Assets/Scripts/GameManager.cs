0867bc956a22b0cf224f860413b3346627ad499f
Henry <tongsik98@gmail.com>
2025-10-09 19:48:20 -0700

    optimized gama manager script

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,45 +19,19 @@ public class GameManager : MonoBehaviour
     {
         CreateDeck();
         ShuffleDeck();
-        DealPlayerCards();
-        DealCommunityCards();
+        DealHoleCards();
+        DealCommunityCards(3);
     }
 
     public void ResetButton()
     {
-        ClearPlayerCardHolder();
-        ClearCommunityHolder();
-
-        playerCards.Clear();
-        opponentCards.Clear();
-        communityCards.Clear();
-
+        ClearAllCardHolders();
         ShuffleDeck();
-        DealPlayerCards();
-        DealCommunityCards();
+        DealHoleCards();
+        DealCommunityCards(5);
     }
 
-    // ============================== functions for reset ==============================
-    void ClearPlayerCardHolder()
-    {
-        for (int i = playerCardsHolder.childCount - 1; i >= 0; i--)
-        {
-            DestroyImmediate(playerCardsHolder.GetChild(i).gameObject);
-        }
-
-        for (int i = opponentCardsHolder.childCount - 1; i >= 0; i--)
-        {
-            DestroyImmediate(opponentCardsHolder.GetChild(i).gameObject);
-        }
-    }
-    void ClearCommunityHolder()
-    {
-        for (int i = communityCardsHolder.childCount - 1; i >= 0; i--)
-        {
-            DestroyImmediate(communityCardsHolder.GetChild(i).gameObject);
-        }
-    }
-
+    // this will execute only one time at the very beginning
     void CreateDeck()
     {
         int spriteIndex = 0;
@@ -68,11 +42,8 @@ public class GameManager : MonoBehaviour
                 CardData cardData = ScriptableObject.CreateInstance<CardData>();
                 cardData.suit = suit;
                 cardData.rank = rank;
+                cardData.cardImage = cardSprites[spriteIndex];
 
-                if (spriteIndex < cardSprites.Length)
-                {
-                    cardData.cardImage = cardSprites[spriteIndex];
-                }
 
                 deck.Add(cardData);
                 spriteIndex++;
@@ -80,6 +51,35 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    // this will execute every time when reset
+    void ClearAllCardHolders()
+    {
+        // cards in holders go back to deck
+        deck.AddRange(playerCards);
+        deck.AddRange(opponentCards);
+        deck.AddRange(communityCards);
+
+        // destroying game objects
+        for (int i = playerCardsHolder.childCount - 1; i >= 0; i--)
+        {
+            Destroy(playerCardsHolder.GetChild(i).gameObject);
+        }
+
+        for (int i = opponentCardsHolder.childCount - 1; i >= 0; i--)
+        {
+            Destroy(opponentCardsHolder.GetChild(i).gameObject);
+        }
+
+        for (int i = communityCardsHolder.childCount - 1; i >= 0; i--)
+        {
+            Destroy(communityCardsHolder.GetChild(i).gameObject);
+        }
+
+        // clearing the lists
+        playerCards.Clear();
+        opponentCards.Clear();
+        communityCards.Clear();
+    }
     void ShuffleDeck()
     {
         for (int i = 0; i < deck.Count; i++)
@@ -90,95 +90,60 @@ public class GameManager : MonoBehaviour
             deck[randomIndex] = temp;
         }
     }
-    // ============================== /functions for reset ==============================
 
     // ============================== deal cards ==============================
-    void DealPlayerCards()
-    {
-        for (int i = 0; i < 2; i++)
-        {
-            if (deck.Count > 0)
-            {
-                // Player card
-                CardData drawnCard = deck[0];
-                deck.RemoveAt(0);
-                playerCards.Add(drawnCard);
-                DisplayCard(drawnCard, playerCardsHolder);
-
-                // Opponent card
-                CardData drawnCardOpponent = deck[0];
-                deck.RemoveAt(0);
-                opponentCards.Add(drawnCardOpponent);
-                DisplayCard(drawnCardOpponent, opponentCardsHolder);
-            }
-            else
-            {
-                Debug.Log("Deck is empty!");
-            }
-        }
-    }
-
-    void DealCommunityCards()
-    {
-        for (int i = 0; i < 5; i++)
-        {
-            if (deck.Count > 0)
-            {
-                CardData drawnCard = deck[0];
-                deck.RemoveAt(0);
-                communityCards.Add(drawnCard);
-                DisplayCommunityCard(drawnCard);
-
-                //Debug.Log($"Community Card {i + 1}: {drawnCard.rank} of {drawnCard.suit} (Value: {(int)drawnCard.rank})");
-            }
-            else
-            {
-                Debug.Log("Deck is empty!");
-                break;
-            }
-        }
-        //Debug.Log($"Community cards dealt: {communityCards.Count}. Remaining deck: {deck.Count}");
-    }
-    // ============================== /deal cards ==============================
-
-    // ============================== display cards ==============================
+    
+    // render cards
     void DisplayCard(CardData cardData, Transform holder)
     {
         GameObject cardObject = Instantiate(cardPrefab, holder);
         CardUI cardComponent = cardObject.GetComponent<CardUI>();
-
         if (cardComponent != null)
         {
             cardComponent.Setup(cardData);
         }
     }
-
-    void DisplayCommunityCard(CardData cardData)
+    private CardData DrawCard()
     {
-        GameObject cardObject = Instantiate(cardPrefab, communityCardsHolder);
-        CardUI cardComponent = cardObject.GetComponent<CardUI>();
+        CardData card = deck[0];
+        deck.RemoveAt(0);
+        return card;
+    }
 
-        if (cardComponent != null)
+    // pre-flop
+    void DealHoleCards()
+    {
+        for (int i = 0; i < 2; i++)
         {
-            cardComponent.Setup(cardData);
+            // Player
+            CardData playerCard = DrawCard();
+            playerCards.Add(playerCard);
+            DisplayCard(playerCard, playerCardsHolder);
+
+            // Opponent
+            CardData opponentCard = DrawCard();
+            opponentCards.Add(opponentCard);
+            DisplayCard(opponentCard, opponentCardsHolder);
         }
     }
-    // ============================== /display cards ==============================
 
-    // Player cards
-    public List<CardData> GetAllPlayerCards()
+    // deal 3, 1, 1
+    void DealCommunityCards(int num)
     {
-        List<CardData> allCards = new List<CardData>();
-        allCards.AddRange(playerCards);
-        allCards.AddRange(communityCards);
-        return allCards;
+        for (int i = 0; i < num; i++)
+        {
+            CardData card = DrawCard();
+            communityCards.Add(card);
+            DisplayCard(card, communityCardsHolder);
+        }
     }
+    // ============================== /deal cards ==============================
 
-    // Opponent cards
-    public List<CardData> GetAllOpponentCards()
+
+    public List<CardData> GetAllCards(List<CardData> holeCards)
     {
         List<CardData> allCards = new List<CardData>();
-        allCards.AddRange(opponentCards);
+        allCards.AddRange(holeCards);
         allCards.AddRange(communityCards);
         return allCards;
     }
