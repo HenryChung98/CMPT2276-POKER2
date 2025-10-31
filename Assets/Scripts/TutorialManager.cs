49b5b8f7e857ebf9a319d2587e079d78f2279f93
Henry <tongsik98@gmail.com>
2025-10-31 16:18:07 -0700

    feat: add some speech bubble objects in tutorial scene and some code to handle them in tutorial manager script

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,5 +1,8 @@
 using UnityEngine;
+using UnityEngine.UI;
 
+
+// you can add more states or rename them if you want.
 enum TutorialState
 {
     first,
@@ -10,17 +13,41 @@ enum TutorialState
 public class TutorialManager : MonoBehaviour
 {
     private TutorialState currentState = TutorialState.first;
+    public Image[] speechBubbles; // create and add speech bubble at the inspector as you need
 
+    private void Start()
+    {
+        foreach (var bubble in speechBubbles)
+        {
+            bubble.gameObject.SetActive(false);
+        }
+        speechBubbles[0].gameObject.SetActive(true);
+
+    }
     private void RenderTutorial()
     {
+        // Deactivate all speech bubbles first
+        foreach (var bubble in speechBubbles)
+        {
+            bubble.gameObject.SetActive(false);
+        }
+
+        // Activate only the speech bubble based on the current TutorialState.
         switch (currentState)
         {
             case TutorialState.first:
                 Debug.Log("welcome");
+                speechBubbles[0].gameObject.SetActive(true);
                 break;
 
             case TutorialState.second:
                 Debug.Log("second page");
+                speechBubbles[1].gameObject.SetActive(true);
+                break;
+
+            case TutorialState.third:
+                Debug.Log("third page");
+                speechBubbles[2].gameObject.SetActive(true);
                 break;
 
 
